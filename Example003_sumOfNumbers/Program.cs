3ac50ca1eddb99584d3f7e01420249407a6d9f8d
chirkova marina <theomarineo@gmail.com>
2022-12-10 23:25:52 +0300

    I added rndom fuction in example003

--- Example003_sumOfNumbers/Program.cs
+++ Example003_sumOfNumbers/Program.cs
@@ -1,3 +1,6 @@
-﻿int firstNumber = 3;
-int secondNumber = 5;
-Console.WriteLine(firstNumber + secondNumber);
+﻿int firstNumber = new Random().Next(1, 10);
+Console.WriteLine(firstNumber);
+int secondNumber = new Random().Next(1, 10);
+Console.WriteLine(secondNumber);
+int result = firstNumber + secondNumber;
+Console.WriteLine(result);
