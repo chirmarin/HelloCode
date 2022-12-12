001483184211aaa8abe0749eea27c971e7019a81
chirkova marina <theomarineo@gmail.com>
2022-12-12 22:03:24 +0300

    exmpl010 about methods of array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {1,2,3,4,5,66,7,8,119,66};
+
+int n = array.Length;
+int find = 66;
+
+int index = 0;
+
+while(index < n) {
+    if(array[index] == find){
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
